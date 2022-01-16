fdc202fbc0fe6b4902a14dc80c7975b03a7eaacd
adsonb87 <adsonb87@gmail.com>
2022-01-16 18:58:23 -0300

    Aula

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 const { Router, response } = require('express');
 const DevController = require('./controllers/Devcontroller');
+const SearchController = require('./controllers/SearchController');
 
 const routes = Router();
 
 routes.get('/devs', DevController.index);
 routes.post('/devs', DevController.store);
+
+routes.get('/search', DevController.index);
 
 module.exports = routes;
